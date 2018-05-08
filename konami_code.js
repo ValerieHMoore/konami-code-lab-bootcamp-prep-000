f924998b4351467e5a5e3fd66b5c8203d8b209c0
Valerie Moore <periwinkle17@outlook.com>
2018-05-08 18:15:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,9 +12,18 @@ const codes = [
 ];
 
 function init() {
-  return init
+ function helperFunction(element) {
+    if (element.children.length > 0) {
+    return helperFunction(element.children[0])
+  } else {
+    return element
+  }
+}
+ return init
   function alert(codes){
-  if codes.length === 10, "Congrats!"}
+  if (codes.length === 10) {
+    return "Congrats!"
+  } else {
   document.body.addEventListener('keydown',alert) {
   } 
 }
